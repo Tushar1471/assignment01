22b4981337fba2515ef7be3b91646a8a7bedc2ab
TUSHAR MALHOTRA <126563540+Tushar1471@users.noreply.github.com>
2023-05-20 18:26:23 +0530

    Add files via upload

---
+++ letsupgrad assignments/Assignment2.js
@@ -0,0 +1,7 @@
+//1.Create an array called "fruits" with the following fruits: "apple", "banana", "orange", "grape", "mango".
+
+// 2.Use JavaScript to display the total number of fruits in the array
+
+const fruits = ["apple", "banana", "orange", "grape", "mango"];
+console.log(fruits);
+console.log(fruits.length);
