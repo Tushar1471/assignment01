0302dc7ddf78c9922edcf16d9ff85c0f36bfff8e
TUSHAR MALHOTRA <126563540+Tushar1471@users.noreply.github.com>
2023-05-20 17:47:34 +0530

    Add files via upload

---
+++ letsupgrad assignments/script.js
@@ -0,0 +1,9 @@
+//Create a function that takes two numbers as parameters and returns their sum.
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(5, 6));
+console.log(sum(9, 5));
+console.log(sum(2, 7));
